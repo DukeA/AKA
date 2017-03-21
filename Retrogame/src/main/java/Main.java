913a672e9a0f8019e4b19527e4fa6d10ce7e98c6
DukeA <adam.granden@hotmail.com>
2017-03-21 10:27:50 +0100

    first commit

---
+++ Retrogame/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Adam on 2017-03-21.
+ */
+public class Main extends Application {
+    public static  void main(String[] args) {
+
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
