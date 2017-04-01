cb2375bfc632726ab80d63bb30a241e6f9adc24e
Yeriza <toxicalle96@gmail.com>
2017-04-01 14:58:11 +0200

    added getters and setters, completed the shell of the constructor

--- RoungOut/core/src/Objects/Pad.java
+++ RoungOut/core/src/Objects/Pad.java
@@ -1,16 +1,42 @@
 package Objects;
 
 /**
- * Created by Deltagare on 2017-04-01.
+ * Created by Alex on 2017-04-01.
  */
 public class Pad {
 
     //Variables for objects
-    float Length;
-    float Height;
-    float PadXPos;
-    float PadYPos;
+    private float length;
+    private float height;
+    private float padXPos;
+    private float padYPos;
 
-    public Pad() {
+    //Getters
+    public float getLength() {
+        return length;
+    }
+    public float getHeight() {
+        return height;
+    }
+    public float getPadXPos() {
+        return padXPos;
+    }
+    public float getPadYPos() {
+        return padYPos;
+    }
+
+    //Setters
+    public void setPadXPos(float padXPos) {
+        this.padXPos = padXPos;
+    }
+    public void setPadYPos(float padYPos) {
+        this.padYPos = padYPos;
+    }
+
+    public Pad(float length, float height, float padXPos, float padYPos) {
+        this.length = length;
+        this.height = height;
+        this.padXPos = padXPos;
+        this.padYPos = padYPos;
     }
 }
