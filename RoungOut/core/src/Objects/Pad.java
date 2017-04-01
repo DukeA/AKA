f15ac10daf955beaa57731455c1c4287ba072dd5
Yeriza <toxicalle96@gmail.com>
2017-04-01 14:44:51 +0200

    Created Class Pad, added constructor shell, added some variables

---
+++ RoungOut/core/src/Objects/Pad.java
@@ -0,0 +1,16 @@
+package Objects;
+
+/**
+ * Created by Deltagare on 2017-04-01.
+ */
+public class Pad {
+
+    //Variables for objects
+    float Length;
+    float Height;
+    float PadXPos;
+    float PadYPos;
+
+    public Pad() {
+    }
+}
