73206746a59a647222e5d8f694aad3b424b0b3cc
Yeriza <toxicalle96@gmail.com>
2017-04-28 18:18:07 +0200

    Implemented a shell of the libgdx interface InputProcessor (it is what we are going to use to check for inputs)

--- RoungOut/core/src/Controller/PlayerController.java
+++ RoungOut/core/src/Controller/PlayerController.java
@@ -1,18 +1,55 @@
 package Controller;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 
-import java.awt.event.ActionListener;
 
 /**
  * Created by Alex on 2017-04-28.
  */
-public class PlayerController implements IPlayerController {
+public class PlayerController implements IPlayerController, InputProcessor{
 
+    //KeyPressed returns a char, its used in the IPlayerController to signal inputs to other parts of the program
+    @Override
     public char KeyPressed(char x){
         return x;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 
     boolean W = Gdx.input.isKeyPressed(Input.Keys.W);
