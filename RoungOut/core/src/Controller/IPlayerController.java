e489f6b02509c4a8273ccaeca2e1bf72d18d962b
Yeriza <toxicalle96@gmail.com>
2017-04-28 17:42:55 +0200

    added IPlayerController shell, made some experimental code with libgdx library, looking into how the keyinputs should work

---
+++ RoungOut/core/src/Controller/IPlayerController.java
@@ -0,0 +1,9 @@
+package Controller;
+
+/**
+ * Created by Deltagare on 2017-04-28.
+ */
+public interface IPlayerController {
+
+    char KeyPressed(char x);
+}
--- RoungOut/core/src/Controller/PlayerController.java
+++ RoungOut/core/src/Controller/PlayerController.java
@@ -1,7 +1,20 @@
 package Controller;
 
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
+import java.awt.event.ActionListener;
+
 /**
  * Created by Alex on 2017-04-28.
  */
-public class PlayerController {
+public class PlayerController implements IPlayerController {
+
+    public char KeyPressed(char x){
+        return x;
+    }
+
+    boolean W = Gdx.input.isKeyPressed(Input.Keys.W);
+    boolean S = Gdx.input.isKeyPressed(Input.Keys.S);
 }
