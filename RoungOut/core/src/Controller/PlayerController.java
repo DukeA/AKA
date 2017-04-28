0e32edbb3883ae82403b28ae99189d13604062c1
Yeriza <toxicalle96@gmail.com>
2017-04-28 18:49:32 +0200

    Changed the controls from up and down to left and right, need to discuss if we can use the input library to map ints to keys on other places in the program

--- RoungOut/core/src/Controller/PlayerController.java
+++ RoungOut/core/src/Controller/PlayerController.java
@@ -12,13 +12,38 @@ public class PlayerController implements IPlayerController, InputProcessor{
 
     //KeyPressed returns a char, its used in the IPlayerController to signal inputs to other parts of the program
     @Override
-    public char KeyPressed(char x){
+    public char keyWasPressed(char x){
         return x;
     }
 
     @Override
     public boolean keyDown(int keycode) {
-        return false;
+        if (keycode == Input.Keys.A){
+            keyWasPressed('A');
+        }
+
+        if (keycode == Input.Keys.D){
+            keyWasPressed('D');
+        }
+
+        if (keycode == Input.Keys.DPAD_LEFT){
+            keyWasPressed('V'); //U since upp arrow char doesn't exist
+        }
+
+        if (keycode == Input.Keys.DPAD_RIGHT){
+            keyWasPressed('H');  //N since upp arrow char doesn't exist
+        }
+
+        /**
+         *  P1:WASD          P2:UNVH (Upp, Ner, Vnster, Hger)
+         *      W                   U
+         *  A   S   D           V   N   H
+         *
+         *  It would be much simpler if the return value of the interfaced could be an int and
+         *  simply import the "com.badlogic.gdx.Input" library to reach the Inputs.Keys field
+         */
+
+        return true;
     }
 
     @Override
