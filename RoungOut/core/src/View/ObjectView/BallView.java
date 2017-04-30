d5ba5611e03d91543042c6ad95138cd19deddaa6
DukeA <adam.granden@hotmail.com>
2017-04-30 15:44:32 +0200

    Commit second

--- RoungOut/core/src/View/ObjectView/BallView.java
+++ RoungOut/core/src/View/ObjectView/BallView.java
@@ -1,7 +1,49 @@
 package View.ObjectView;
 
+import com.badlogic.gdx.Screen;
+
 /**
  * Created by DukeA on 2017-04-28.
  */
-public class BallView  {
+public class BallView implements Screen {
+
+    public BallView() {
+        
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
--- RoungOut/core/src/View/ObjectView/BoardView.java
+++ RoungOut/core/src/View/ObjectView/BoardView.java
@@ -17,6 +17,8 @@ public class BoardView implements Screen {
     private final Roungout game;
     private com.badlogic.gdx.scenes.scene2d.Stage stage;
 
+    private BallView ballView;
+
     public BoardView( final Roungout game){
         this.game = game;
         this.stage = new com.badlogic.gdx.scenes.scene2d.Stage(new FitViewport(Roungout.WIDTH,Roungout.WIDTH,game.camera));
@@ -31,14 +33,18 @@ public class BoardView implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1f,1f,1f,1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        update(delta);
+
+        stage.draw();
     }
     public void update(float delta) {
-
+            stage.act(delta);
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width,height,false);
+        stage.getViewport().update(width,height,true);
     }
 
     @Override
