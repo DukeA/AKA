3288730d217e83f3dcb40071fa3c4faec3fa2628
DukeA <adam.granden@hotmail.com>
2017-05-01 16:39:06 +0200

    Commit final

--- RoungOut/core/src/View/ObjectView/BallView.java
+++ RoungOut/core/src/View/ObjectView/BallView.java
@@ -21,46 +21,51 @@ public class BallView implements Screen {
     private Stage stage;
     private SpriteBatch batch;
     private ShapeRenderer shapeRenderer;
-
+    private OrthographicCamera camera;
 
     public BallView(int WIDTH, int HEIGHT, OrthographicCamera camera) {
-
-        model = new Ball(WIDTH / 2, HEIGHT / 2, 10, 1, 1);
+        this.camera = camera;
+        model = new Ball(WIDTH /    2, HEIGHT / 2, 10, 10, 10);
         this.stage = new Stage(
                 new FitViewport(WIDTH / 2 + 10
-                        , HEIGHT / 2 + 10,camera));
+                        , HEIGHT / 2 + 10, camera));
     }
 
     @Override
     public void show() {
-        batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
-
-        batch.begin();
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.RED);
-        shapeRenderer.ellipse((float)model.getX()-200,(float)model.getY()-450
-                ,100f, (float)( 50f * model.getY()/model.getX()));
+        shapeRenderer.ellipse((float) ( model.getX() - 200+model.getSpeed()), (float) (model.getY() - 450 +model.getSpeed())
+                , 100f, (float) (50f * model.getY() / model.getX()));
         shapeRenderer.end();
-        batch.end();
     }
 
     @Override
     public void render(float delta) {
-    Gdx.gl.glClearColor(1f,1f,1f,1f);
-    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-    update(delta);
-    show();
-    stage.act();
-    stage.draw();
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        update(delta);
+        batch = new SpriteBatch();
+
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        show();
+        batch.end();
+        stage.act();
+        stage.draw();
     }
+
     public void update(float delta) {
         stage.act(delta);
+        model.setPosition(model.getX() + model.getSpeed()
+                ,model.getY()+model.getSpeed());
+        camera.update();
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width,height,true);
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
