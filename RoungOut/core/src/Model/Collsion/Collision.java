d3f4c64d74c9ea6b25e3b18bb0914e40a1f2ba60
gusbacke <gusbacke@users.noreply.github.com>
2017-05-01 17:57:56 +0200

    Collision class can now check board collisions

--- RoungOut/core/src/Model/Collsion/Collision.java
+++ RoungOut/core/src/Model/Collsion/Collision.java
@@ -1,15 +1,28 @@
 package Model.Collsion;
 
+import Model.GameObjects.Ball;
 import Model.GameObjects.Physics.Body;
-import Model.ViewObjects.IBoard;
+import Model.GameObjects.Physics.CircleBody;
+import Model.ViewObjects.Board;
 
 /**
  * @author Ken Bäcklund
  */
 public class Collision {
 
+    private CircleBody board;       // TODO:  Temporary solution, will use proper Board class later.
+
+    public Collision() {
+        board = new CircleBody(0f, 0f, 1000f);  // TODO: Use proper board class
+    }
+
     public boolean isCollision(Body someBody, Body anyBody) {
         return (someBody.distance(anyBody) <= 0.0001f);
+    }
+
+    public boolean isOutsideBoardRange(Ball ball, double boardRadius) {
+        board.setRadius(boardRadius);   // TODO: Temporary solution
+        return (ball.distance(board) >= 0.0001f);
     }
 
 }
