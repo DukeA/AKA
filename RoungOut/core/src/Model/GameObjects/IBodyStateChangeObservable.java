89196163c6e2438ddb2b0cbc50f48d9431954b6a
gusbacke <gusbacke@users.noreply.github.com>
2017-05-02 09:51:36 +0200

    Added initial Observer interfaces for BodyStateChange.

---
+++ RoungOut/core/src/Model/GameObjects/IBodyStateChangeObservable.java
@@ -0,0 +1,16 @@
+package Model.GameObjects;
+
+/**
+ * Makes a Body instance observable.
+ * Should trigger by any changes such as position, speed or directional changes.
+ *
+ * Observers are notified using the following method:
+ *     void onBodyStateChange(Body body)
+ *
+ * @author Ken Bäcklund
+ */
+public interface IBodyStateChangeObservable {
+    void addListener(IBodyStateChangeObservable observable);
+    void removeListener(IBodyStateChangeObservable observable);
+
+}
