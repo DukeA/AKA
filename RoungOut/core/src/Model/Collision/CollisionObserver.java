b80e52a78a53318348336dac66e16d19cf638ae3
gusbacke <gusbacke@student.gu.se>
2017-05-26 18:10:13 +0200

    Added javadoc comments to the CollisionObserver and CollisionObservable interfaces.

--- RoungOut/core/src/Model/Collision/CollisionObserver.java
+++ RoungOut/core/src/Model/Collision/CollisionObserver.java
@@ -5,13 +5,36 @@ import Model.GameObjects.Brick;
 import Model.GameObjects.Player;
 
 /**
- * Interface for observing collisions
+ * Interface for observing board collisions
  *
  * @author Ken Bäcklund
  */
 public interface CollisionObserver {
+
+    /**
+     * Observer method listening to when a ball exits the board.
+     * @param lostBall the ball that got lost.
+     */
     void onBallExitBoard(Ball lostBall);
+
+    /**
+     * Observer method for listening to when a ball collides with another ball.
+     * @param ball the ball that got hit.
+     * @param otherBall the other ball that got hit.
+     */
     void onBallHitBall(Ball ball, Ball otherBall);
+
+    /**
+     * Observer method for listening to when a ball hits a brick.
+     * @param ball the ball that hit the brick.
+     * @param brick the brick that got hit.
+     */
     void onBallHitBrick(Ball ball, Brick brick);
+
+    /**
+     * Observer method for listening to when a ball hits a player (and it's related pad).
+     * @param ball the ball that hit the player pad.
+     * @param player the player and it's related pad that got hit.
+     */
     void onBallHitPlayerPad(Ball ball, Player player);
 }
