1e16eb88642bc86ff2d3835e9ca4ec8121c67681
Yeriza <toxicalle96@gmail.com>
2017-05-27 13:28:30 +0200

    - Added comments and author tags in the AbstractGameComponents folder

--- RoungOut/core/src/AbstractGameComponents/AGame.java
+++ RoungOut/core/src/AbstractGameComponents/AGame.java
@@ -4,11 +4,30 @@ import Model.GameObjects.Board;
 import com.badlogic.gdx.Game;
 
 /**
- * Created by Alex on 2017-05-20.
+ *  @author Alex
+ *  Created on 2017-05-20.
  */
 public abstract class AGame extends Game {
+    /**
+     * @return Returns a board
+     */
     public abstract Board getBoard ();
+
+    /**
+     *
+     * @return Returns an abstract gameController
+     */
     public abstract AGameController getGameController();
+
+    /**
+     *
+     * @return Returns an abstract menuController
+     */
     public abstract AMenuController getMenuController();
+
+    /**
+     *
+     * @return Returns an abstract optionsController
+     */
     public abstract AOptionsController getOptionsController();
 }
--- RoungOut/core/src/AbstractGameComponents/AMenuController.java
+++ RoungOut/core/src/AbstractGameComponents/AMenuController.java
@@ -4,10 +4,28 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
- * Created by DukeA on 2017-05-24.
+ * @author DukeA
+ * Created on 2017-05-24.
  */
 public abstract class AMenuController  extends ClickListener implements InputProcessor{
+    /**
+     *
+     * @param Width Width of the screen
+     * @param Height Height of the screen
+     * @param game the game we are currently using
+     */
     public abstract void playButtonIsPressed(int Width, int Height, AGame game);
+
+    /**
+     *
+     *  @param Width Width of the screen
+     * @param Height Height of the screen
+     * @param game the game we are currently using
+     */
     public abstract void optionsButtonIsPressed(int Width, int Height, AGame game);
+
+    /**
+     * Exits the applications i the implementation of the method
+     */
     public abstract void exitButtonIsPressed();
 }
