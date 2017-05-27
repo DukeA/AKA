80bc3c17995a40637bead445bb9662b94b87af6a
Yeriza <toxicalle96@gmail.com>
2017-05-27 21:53:51 +0200

    -Commented RoungOut

--- RoungOut/core/src/prototype/src/desktop/Roungout.java
+++ RoungOut/core/src/prototype/src/desktop/Roungout.java
@@ -1,32 +1,35 @@
 package prototype.src.desktop;
 
-/*<<<<<<< HEAD
-import Controller.*;
+/**
+ * @author Adam
+ * @author Ken
+ * @author Alex
+ * Created at the start of the project, consistently modified during the duration of the project
+ */
 
-import AbstractGame.AGame;
-=======*/
 import AbstractGameComponents.AGameController;
 import AbstractGameComponents.AMenuController;
 import AbstractGameComponents.AOptionsController;
 import Controller.*;
 
 import AbstractGameComponents.AGame;
-//>>>>>>> Change_Controller
 import Model.GameObjects.*;
 import View.MenuView.SplashView;
 import View.ObjectView.BoardView;
 import IViews.IViews;
 import Model.GameObjects.IPlayer;
-import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 import java.util.List;
 
 
 public class Roungout extends AGame {
+    /**
+     * The initializer of the whole game, it is here everything in the game start
+     */
+
 
     public static final String TITLE = "Roungout";
     public static final float VERSION = 0.3f;
@@ -41,60 +44,47 @@ public class Roungout extends AGame {
     private ArrayList<IViews> menuControllerViewers = new ArrayList<IViews>();
     private ArrayList<IViews> optionsControllerViewers = new ArrayList<IViews>();
 
-/*<<<<<<< HEAD
-    //create controllers
-    public GameController gameController;
-    public OptionsController optionsController;
-    public MenuController menuController;
-
-
-    private ArrayList<IViews> viewers = new ArrayList<IViews>();
-
-
-    private BoardView view;
-=======*/
     private BoardView BoardView;
-//>>>>>>> Change_Controller
     private Board board;
 
     private GameController gameController;
     private MenuController menuController;
     private OptionsController optionsController;
 
+    /**
+     * Getter, gets the board
+     * @return Board
+     */
     public Board getBoard(){
         return board;
     }
-/*<<<<<<< HEAD
-    public GameController getGameController(){
-        return gameController;
-    }
-    public OptionsController getOptionsController() {return optionsController;}
-    public MenuController getMenuController () {return menuController;}
 
-   /* public void initControllers() {
-        List<IPlayer> players = new ArrayList<IPlayer>(board.getPlayers());
-
-        ControllerHandler handler = new ControllerHandler();
-
-        GameController gameController = new GameController(viewers,players.get(0),players.get(1),handler);
-        ArrayList<IControllHandeling> controllers = new ArrayList<IControllHandeling>();
-
-        controllers.add(gameController);
-        //Init the handler with the controllers
-        handler.setControllers(controllers);
-    }*/
-
-//=======
+    /**
+     * Getter, gets the gameController
+     * @return The gameController
+     */
     @Override
     public AGameController getGameController(){
         return gameController;
     }
+
+    /**
+     * Getter, gets the menuController
+     * @return The menuController
+     */
     @Override
     public AMenuController getMenuController() {return menuController;}
+
+    /**
+     * Getter, gets the menuController
+     * @return The menuController
+     */
     @Override
     public AOptionsController getOptionsController() {return optionsController;}
-//>>>>>>> Change_Controller
 
+    /**
+     * Part of the framework, this happens the when the Class is created (Comes from AGame, AGame extends this from Game)
+     */
     @Override
     public void create() {
 
@@ -102,32 +92,22 @@ public class Roungout extends AGame {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, WIDTH, HEIGHT);
         board = new Board(WIDTH, HEIGHT);
-/*<<<<<<< HEAD
-        view = new BoardView(WIDTH, HEIGHT,this);
-        viewers.add(view);
-=======*/
+
         //board.createSampleBoard(WIDTH, HEIGHT);         // Sample board creation here. Otherwise BoardTest is screwed up.
         BoardView = new BoardView(WIDTH, HEIGHT,this);
         gameControllerViewers.add(BoardView);
 
-//>>>>>>> Change_Controller
 
 
         //InitControllers
         //Init handler
         ControllerHandler handler = new ControllerHandler();
-/*<<<<<<< HEAD
 
-        //init gameController
-        List<IPlayer> players = new ArrayList<IPlayer>(board.getPlayers());
-        gameController = new GameController(viewers,players.get(0),players.get(1),handler);
-=======*/
         List<IPlayer> players = new ArrayList<IPlayer>(board.getPlayers());
         gameController = new GameController(gameControllerViewers,players.get(0),players.get(1),handler);
         menuController = new MenuController(menuControllerViewers,handler);
         optionsController = new OptionsController(optionsControllerViewers,handler);
 
-//>>>>>>> Change_Controller
 
 
         //put all controllers in a list to the handler
@@ -138,22 +118,21 @@ public class Roungout extends AGame {
         //Init the handler with the controllers
         handler.setControllers(controllers);
 
-/*<<<<<<< HEAD
-        //Sets the input processor to this index (GAME CONTROLLER)
-        handler.getcontrollers().get(0).changeInputProcessor();
 
-
-=======*/
         splashScreen = new SplashView(WIDTH, HEIGHT, this); //set input
-//>>>>>>> Change_Controller
         this.setScreen(splashScreen);
     }
 
-
+    /**
+     * Part of the framework, inherited from Game
+     */
     public void render() {
         super.render();
     }
 
+    /**
+     * Part of the framework, inherited from Game
+     */
     public void dispose() {
         super.dispose();
     }
