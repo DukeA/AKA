faf5dbcc395426b1ee18b2145cabf2cae53b8870
Yeriza <toxicalle96@gmail.com>
2017-05-27 22:15:07 +0200

    -Comented the Vector class in the Utils package

--- RoungOut/core/src/Utils/Vector.java
+++ RoungOut/core/src/Utils/Vector.java
@@ -1,7 +1,8 @@
 package Utils;
 
 /**
- * Created by DukeA on 2017-05-20.
+ * @author Adam
+ * Created on 2017-05-20.
  */
 
 /**
@@ -13,20 +14,35 @@ public class Vector {
     private float yPos;
     private float zPos;
 
-
+    /**
+     * Constructor
+     * @param xPos X-Position of the first point
+     * @param yPos Y-Position of the first point
+     * @param xPos2 X-Position of the second point
+     * @param yPos2 Y-Position of the second point
+     */
     public Vector(float xPos, float yPos, float xPos2, float yPos2) {
         this.xPos = xPos - xPos2;
         this.yPos = yPos - yPos2;
-        this.zPos = 0;
+        this.zPos = 0; //2D , zed dimension is not needed
 
     }
 
+    /**
+     * Constructor, the vector is this point and the origin
+     * @param xPos X-Position of the point
+     * @param yPos Y-Position of the point
+     * @param zPos Z-Position of the point
+     */
     public Vector(float xPos, float yPos, float zPos) {
         this.xPos = xPos;
         this.yPos = yPos;
         this.zPos = zPos;
     }
 
+    /**
+     * Clone Constructor
+     */
     public Vector(Vector vector) {
         this.xPos = vector.xPos;
         this.yPos = vector.yPos;
@@ -34,7 +50,8 @@ public class Vector {
     }
 
     /**
-     * The following two classes are to calculate the crossProudct
+     * This method calculates the cross product
+     * @return The cross product
      */
     public Vector crossProudct() {
         Vector vectorZ = new Vector(0, 0, 1);
@@ -43,37 +60,57 @@ public class Vector {
                 this.xPos * vectorZ.yPos - this.yPos * vectorZ.xPos);
     }
     /**
-     * The Normalization of a Vector
+     * This method returns a normalization of a Vector
+     * @return The normalization of the vector
      */
-
     public Vector normalization() {
         double length = Math.sqrt(Math.pow(this.xPos, 2) + Math.pow(this.yPos, 2));
         return new Vector((float) (this.xPos / length), (float) (this.yPos / length)
                 , (float) (this.zPos / length));
     }
 
+    /**
+     * This method given a length of a rectangular shape, returns the vector from the middle point to the edge
+     * (length-wise) of the shape
+     * @param length
+     * @return The vector from the middle point to the edge
+     */
     public Vector vectorLength(float length) {
         return new Vector(this.xPos * length / 2,
                 this.yPos * length / 2,
                 this.zPos * length / 2);
     }
-
+    /**
+     * This method given a width of a rectangular shape, returns the vector from the middle point to the edge
+     * (length-wise) of the shape
+     * @param width
+     * @return The vector from the middle point to the edge
+     */
     public Vector vectorWidth(float width) {
         return new Vector(this.xPos * width / 2,
                 this.yPos * width / 2,
                 this.zPos * width / 2);
     }
 
+    /**
+     * Getter, gets the X-Position of this Vector
+     * @return X-Position
+     */
     public float getxPos() {
         return this.xPos;
     }
 
+    /**
+     * Getter, gets the Y-Position of this Vector
+     * @return Y-Position
+     */
     public float getyPos() {
         return this.yPos;
     }
 
-    public float getzPos() {
-        return this.zPos;
-
-    }
+    /**
+     * Getter, gets the Z-Position of this Vector
+     * @return Z-Position
+     */
+    public float getzPos() {return this.zPos;}
 }
